29805c723f7b5bec3031a24db1ee06cfedc7272d
andrewfurey21 <andrewfurey2003@gmail.com>
2023-12-31 13:51:50 +0000

    added strexpr (just stringify) and panic_if (just assert)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[macro_export]
+macro_rules! strexpr {
+    ($expr:expr) => {
+        stringify!($expr)
+    };
+}
+
+#[macro_export]
+macro_rules! panic_if {
+    ($expr:expr) => {
+        assert!($expr)
+    };
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
+mod panic_if_tests {
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn panic_if_true() {
+        panic_if!(true);
+    }
+}
+
+#[cfg(test)]
+mod strexpr_tests {
+    #[test]
+    fn strexpr_true() {
+        let string = strexpr!(1+2+3);
+        assert_eq!(String::from("1 + 2 + 3"), string);
     }
 }
