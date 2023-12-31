6fcdcb718d22f4ee8bf4050670a62a50933a8cf9
andrewfurey21 <andrewfurey2003@gmail.com>
2023-12-31 17:00:25 +0000

    panic_eq + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,34 @@
-#[macro_export]
 macro_rules! strexpr {
-    ($expr:expr) => {
-        stringify!($expr)
+    () => {};
+    ($arg1:expr, $arg2:expr, eq) => {
+        format!("{:?}\nlhs: {:?}\nrhs: {:?}", stringify!($arg1 == $arg2), $arg1, $arg2)
     };
 }
 
 #[macro_export]
-macro_rules! panic_if {
-    ($expr:expr) => {
-        assert!($expr)
+macro_rules! panic_eq {
+    ($arg1:expr, $arg2:expr) => {
+        if $arg1 == $arg2 {
+            panic!("{}", strexpr!($arg1, $arg2, eq))
+        }
     };
 }
 
 #[cfg(test)]
 mod panic_if_tests {
     #[test]
-    fn panic_if_true() {
-        panic_if!(true);
+    #[should_panic]
+    fn panic_if_a_equals_b_should_panic() {
+        let a = 10;
+        let b = 10;
+        let c = 0;
+        panic_eq!(a + c, b);
     }
-}
 
-#[cfg(test)]
-mod strexpr_tests {
     #[test]
-    fn strexpr_true() {
-        let string = strexpr!(1+2+3);
-        assert_eq!(String::from("1 + 2 + 3"), string);
+    fn panic_if_a_equals_b() {
+        let a = 10;
+        let b = 0;
+        panic_eq!(a, b);
     }
 }
